c155b488739c9b097c48adaf633adddabe47a2d6
emanherawy <eman.herawy@gmail.com>
2024-04-21 01:24:55 +0200

    update the legacy code

--- src/agent.ts
+++ src/agent.ts
@@ -11,12 +11,14 @@ import {
   FindingSeverity,
   FindingType,
 } from "forta-agent";
-
-export const ERC20_TRANSFER_EVENT =
-  "event Transfer(address indexed from, address indexed to, uint256 value)";
-export const TETHER_ADDRESS = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
-export const TETHER_DECIMALS = 6;
+import {
+  ERC20_TRANSFER_EVENT,
+  TETHER_ADDRESS,
+  TETHER_DECIMALS,
+} from "./helpers";
 let findingsCount = 0;
+const OneDayInSecs = 86400;
+let nextBatchTime = 0;
 
 const handleTransaction: HandleTransaction = async (
   txEvent: TransactionEvent
@@ -26,43 +28,45 @@ const handleTransaction: HandleTransaction = async (
   // limiting this agent to emit only 5 findings so that the alert feed is not spammed
   if (findingsCount >= 5) return findings;
 
-  // filter the transaction logs for Tether transfer events
-  const tetherTransferEvents = txEvent.filterLog(
-    ERC20_TRANSFER_EVENT,
-    TETHER_ADDRESS
-  );
+  // // filter the transaction logs for Tether transfer events
+  // const tetherTransferEvents = txEvent.filterLog(
+  //   ERC20_TRANSFER_EVENT,
+  //   TETHER_ADDRESS
+  // );
 
-  tetherTransferEvents.forEach((transferEvent) => {
-    // extract transfer event arguments
-    const { to, from, value } = transferEvent.args;
-    // shift decimals of transfer value
-    const normalizedValue = value.div(10 ** TETHER_DECIMALS);
+  // tetherTransferEvents.forEach((transferEvent) => {
+  //   // extract transfer event arguments
+  //   const { to, from, value } = transferEvent.args;
+  //   // shift decimals of transfer value
+  //   const normalizedValue = value.div(10 ** TETHER_DECIMALS);
 
-    // if more than 10,000 Tether were transferred, report it
-    if (normalizedValue.gt(10000)) {
-      findings.push(
-        Finding.fromObject({
-          name: "High Tether Transfer",
-          description: `High amount of USDT transferred: ${normalizedValue}`,
-          alertId: "FORTA-1",
-          severity: FindingSeverity.Low,
-          type: FindingType.Info,
-          metadata: {
-            to,
-            from,
-          },
-        })
-      );
-      findingsCount++;
-    }
-  });
+  //   // if more than 10,000 Tether were transferred, report it
+  //   if (normalizedValue.gt(10000)) {
+  //     findings.push(
+  //       Finding.fromObject({
+  //         name: "High Tether Transfer",
+  //         description: `High amount of USDT transferred: ${normalizedValue}`,
+  //         alertId: "FORTA-1",
+  //         severity: FindingSeverity.Low,
+  //         type: FindingType.Info,
+  //         metadata: {
+  //           to,
+  //           from,
+  //         },
+  //       })
+  //     );
+  //     findingsCount++;
+  //   }
+  // });
 
   return findings;
 };
 
-// const initialize: Initialize = async () => {
-//   // do some initialization on startup e.g. fetch data
-// }
+const initialize: Initialize = async () => {
+  // do some initialization on startup e.g. fetch data
+  // set next batch time 
+  nextBatchTime = Date.now() + OneDayInSecs;
+}
 
 // const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
 //   const findings: Finding[] = [];
@@ -84,7 +88,7 @@ const handleTransaction: HandleTransaction = async (
 // }
 
 export default {
-  // initialize,
+  initialize,
   handleTransaction,
   // healthCheck,
   // handleBlock,
